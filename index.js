061caa00a21fdcdd4b3916f2de28bda7bd13b06d
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-08 02:37:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-// Add your doToElementsInArray() function here:
+var evens = [0, 2, 4, 6, 8, 10];
+
+function evenFn(even, index, array) {
+  console.log(`${even} is not odd!`);
+}
+
+for (let i = 0; i < evens.length; i++) {
+  evenFn(evens[i], i, evens);
+}
+
+function square(n) {
+  console.log(n * n)
+}
 
 
-// Add your changeCompletely() function here:
+function doToEvens(callback) {
+  evens.forEach(callback);
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+// log out animals -- pretty cool, right?
+console.log(animals);
